5effad21cd46d1d05a2bca72f528669da4ad3681
chaudhariayush <122032437+chaudhariayus@users.noreply.github.com>
2024-12-01 02:04:27 +0530

    Delete HealNexus directory

--- HealNexus/Backend/api/routes/Filter.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { filterDoctorsByState, filterDoctorsByCity } from '../controllers/dashboard.controller.js';
-
-const router = express.Router();
-router.get("/search-by-city", filterDoctorsByCity);
-router.get("/search-by-state", filterDoctorsByState);
-
-export default router;
--- HealNexus/Frontend/routes/profile.routes.jsx
+++
@@ -1,7 +0,0 @@
-import {Routes,Route} from "react-router-dom"
-import { ProfileCard } from "../pages/Profile/ProfileCard/ProfileCard"
-export function ProfileRoute(){
-   return <Routes>
-        <Route path="/profile/:role" element={<ProfileCard />} />
-    </Routes>
-}
